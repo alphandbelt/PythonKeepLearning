6a5754c52d58f4cf8ebc725d05166e903e164881
alphandbelt <alphandbelt@gmail.com>
2023-11-28 17:54:41 +0800

    Fix use model

--- Plans/CatPoopRecognition/use_model.py
+++ Plans/CatPoopRecognition/use_model.py
@@ -17,6 +17,7 @@ model = load_model('cat_poop_model.keras')
 # 用于将图像转换为模型输入的预处理函数
 def preprocess_image(img_path):
     img = image.load_img(img_path, target_size=(64, 64))
+
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
